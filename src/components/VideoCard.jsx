a720572acc2af8ecf3b8cb4b5cbdb7019cc33db2
rohan -fa <rohanfarzna@gmail.com>
2023-01-06 23:20:34 +1100

    VideoCard Styling and functioning been done

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -6,14 +6,27 @@ import { demoThumbnailUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle, dem
 const VideoCard = ( {video: { id: {videoId}, snippet}}) => {
 
   return (
-    <Card>
+    <Card sx={{ width: { md: '320px', xs: '100%'}, boxShadow: 'none', borderRadius: 0 }}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>      {/* that link going to wrap the media component   */}{/*found some error in the commandline so inside the link component we will right - to={if videoId exsist? then we will go forward to /video/${videoId} if it doesnot exsist then we want to go to demoVideoUrl   } */}
-           <CardMedia 
+           <CardMedia                   
             image={snippet?.thumbnails?.high?.url}
             alt={snippet?.title}
             sx={{ width: 358, height: 180}}
             />     {/*CardMedia going to have a image property,inside of there we are go into the snippet,? mark dot thumbnails?.high? dot url, make sure to give qurstion marks before dots,else it will come undefine   */}
       </Link>
+      <CardContent sx={{backgroundColor: '#1e1e1e', height: '106px'}}> {/*CardContent material ui content*/}
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+            <Typography varient='subtitle1' forntWeight= 'bold' color='#FFF' >                {/*now lets make it a bit bigger by giving a variant subtitle1*/}
+                {snippet.title.slice(0, 60) || demoVideoTitle.slice(0, 60)} {/*if the title doesnot exist we can render the demo*/}
+            </Typography>
+        </Link>
+        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoVideoUrl}>  {/* snippet question mark dot channel id if its exist, we are going to point channel   */}  
+            <Typography varient='subtitle2' forntWeight= 'bold' color='gray' >                {/*now lets make it a bit bigger by giving a variant subtitle1*/}
+                {snippet?.channelTitle || demoChannelTitle} {/*if the title doesnot exist we can render the demo*/}
+                <CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px'}} />
+            </Typography>
+        </Link>
+      </CardContent> 
     </Card>
   )
 }
