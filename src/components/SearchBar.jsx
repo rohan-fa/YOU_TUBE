dfa21d64f678a180dead052733371d949ac5a5a5
rohan -fa <rohanfarzna@gmail.com>
2023-01-07 22:42:14 +1100

    search input working

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,20 +4,32 @@ import { Paper, IconButton } from '@mui/material';
 import { Search} from '@mui/icons-material';
 
 
-
-
 const SearchBar = () => {
+  const [searchTerm, setsearchTerm] = useState("");
+
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();//because when you submit a form its reload the page/ so now check if the searchTerm exist
+    
+    //if (searchTerm) meaning if something has been typed in?and then if that is the case, we can use, useNavigate hook, we first have to initialize it by saying const navigate... 
+    if(searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setsearchTerm('');//we have to reset the searchTerm to an empty string. 
+    }
+  } 
+
   return (
     <Paper
       component='form'
-      onSubmit={()=>{}}
+      onSubmit={handleSubmit}
       sx={{borderRadius: 20, border: '1px solid #e3e3e3', pl: 2, boxshadow: 'none', mr: {sm: 5} }}
     >
       <input 
       className='search-bar'
       placeholder='Search..'
-      value=''
-      onChange={()=> {}}
+      value={searchTerm}
+      onChange={(e)=> setsearchTerm(e.target.value)}//this will let us type in but the question is how do we make the request to search thing to work to a quary? and we can do that adding handle submit function.  
       />
       <IconButton type='submit' sx={{
         p: '10px', color: 'red'
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Typography} from '@mui/material';
+import { useParams } from "react-router-dom";//after importing in the app we will see how did we call the parameter? searchTerm 
+
+import { Videos } from './index.js'
+import { fetchFromAPI } from "../Utils/fetchFromAPI";
+
+
 
 
 const SearchFeed = () => {
-  return (
-    <div>
-      
-    </div>
+
+  const [videos, setVideos] = useState([]);     //* copied the entire thing from feed, removed the stack portion, removed the selectedCategory *
+  const  {searchTerm } = useParams(); //once we set the quary we pass it through api url,and also dependency array, so that it get updated everytime we seach for something, 
+  //important thing is to figure out how can we fetch the data,regarding our searched videos.so if we go to rapid api, and look for search, search?part=snippet&q=.. this the thing we are already did in the past, now we have to pass the right quary, 'q' in the doc,   
+  useEffect(() => {
+     fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then(
+      (data) => setVideos(data.items)// we should be pulling our data from our params like if we search react build and deploy, in the path their should be localhost:3000/search/build%20and%20deploy%20react%20js, that means we should be pulling our data from parameters to the browser,    
+     )
+  }, [searchTerm]); 
+
+  return (  
+    <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
+      <Typography variant='h4' fontweight='bold' mb={2} sx={{
+        color: 'white'
+      }}>
+        Search Results for :<span style={{color: '#F31503'}}>{searchTerm}</span>Videos
+      </Typography>
+
+      <Videos videos={videos}/>
+   </Box>
   )
 }
 
 export default SearchFeed
+
